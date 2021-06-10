f0d3dfd5521054cfa91a1f45e7366d07a3c2b446
cxcacac <cxcongyeah@126.com>
2021-06-10 14:05:22 +0800

    change parameters

--- Distribute system Analysis/getDamperForce.m
+++ Distribute system Analysis/getDamperForce.m
@@ -2,10 +2,12 @@ function [force] = getDamperForce(x, v, a, choice)
 % non-dimensional in johnson's paper(2007), c = m*w0*L*c_d
 % c_d = 220;
 c_d = 5.07*85*0.6575*220; 
-% in sapinski' paper(2002)
-Fc = 580.31; 
-c0 = 56.77;
-f0 = -176.62;
+%%  in Spencer's paper 1997 
+% use MRX-135GD, Bingham model, 2.5Hz sinusoidal load, constant 1.5V;
+Fc = 670; 
+c0 = 5000;
+f0 = -95;
+%% parameters in different mode;
 if(strcmp(choice, 'passive'))
     force = -c_d*v;
 elseif(strcmp(choice, 'free-vibration'))
